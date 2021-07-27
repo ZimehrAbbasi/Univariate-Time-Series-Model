3988c7f464e31d9dc3d98ceda48247598a80e479
Zimehr Abbasi <57117170+ZimehrAbbasi@users.noreply.github.com>
2021-07-27 00:18:11 -0400

    Create run.py

---
+++ src/run.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from TSModel import TSModel
+import yfinance as yf
+import numpy as np
+
+df = pd.read_csv("DailyDelhiClimateTrain.csv")
+df.index = pd.to_datetime(df["date"].values)
+df = df.drop(columns=['date', "meanpressure", "humidity", "wind_speed"])
+
+df_test = pd.read_csv("DailyDelhiClimateTest.csv")
+df_test.index = pd.to_datetime(df_test["date"].values)
+df_test = df_test.drop(
+    columns=['date', "meanpressure", "humidity", "wind_speed"])
+
+model = TSModel("Y", df, df_test)
+model.train()
+model.test()
+model.predict(3)
+model.show()
