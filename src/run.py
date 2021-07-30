44265dac0dbdde0cb4a295590eb717695df39d94
Zimehr Abbasi <57117170+ZimehrAbbasi@users.noreply.github.com>
2021-07-30 15:24:31 -0400

    Update run.py

--- src/run.py
+++ src/run.py
@@ -3,17 +3,49 @@ from TSModel import TSModel
 import yfinance as yf
 import numpy as np
 
-df = pd.read_csv("../data/DailyDelhiClimateTrain.csv")
+df = pd.read_csv("DailyDelhiClimateTrain.csv")
 df.index = pd.to_datetime(df["date"].values)
 df = df.drop(columns=['date', "meanpressure", "humidity", "wind_speed"])
 
-df_test = pd.read_csv("../data/DailyDelhiClimateTest.csv")
+df_test = pd.read_csv("DailyDelhiClimateTest.csv")
 df_test.index = pd.to_datetime(df_test["date"].values)
 df_test = df_test.drop(
     columns=['date', "meanpressure", "humidity", "wind_speed"])
 
+# df = pd.read_csv("GlobalLandTemperatures_GlobalTemperatures.csv")
+# df.index = pd.to_datetime(df["dt"].values)
+# df = pd.DataFrame(df["LandAverageTemperature"],
+#                   columns=['avg_temp'])
+
+# ticker = "aapl"
+# stock = yf.Ticker(ticker)
+# df = stock.history("10y", "1d")
+# df.index.name = None
+# df = np.log(np.log(df[["Close"]]))
+
+
+# def change(string):
+#     string = str(string)
+#     return string[:4] + "-" + string[4:6] + "-" + string[6:]
+
+
+# def format(df):
+#     lister = df.values.tolist()
+
+#     for i in range(len(lister)):
+#         lister[i] = change(lister[i])
+
+#     return lister
+
+
+# df = pd.read_csv("daily-total-female-births-CA.csv")
+# df.index = pd.to_datetime(df["date"].values)
+# df = df.drop(columns=["date"])
+
 model = TSModel("Y", df, df_test)
-model.train()
+# Number of values in the past to take into consideration
+model.train(period=3)
 model.test()
-model.predict(3)
+# Number of periods in the future to predict
+model.predict(period=3)
 model.show()
